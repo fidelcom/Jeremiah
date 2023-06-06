99a5b92a84edf233521f08a7dea264d89fb54445
Jonathan Jeremiah <110501575+JerryOJJ@users.noreply.github.com>
2023-06-06 10:03:52 +0100

    New Updates

---
+++ app.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+
+    const menu_btn = document.querySelector('.hamburger');
+
+    menu_btn.addEventListener('click', function () {
+        menu_btn.classList.toggle('is-active');
+    });
+
+}
