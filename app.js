4036a49114e8dd2696531bf6f3c9c07f477c6a87
Jonathan Jeremiah <110501575+JerryOJJ@users.noreply.github.com>
2023-07-24 18:06:33 +0100

    Clean up
    Modified features

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /*hamburger = document.querySelector(".hamburger");
 hamburger.onclick =  function(){
-  navBar = document.querySelector(".navbar");
+  navBar = document.querySelector(".navBar");
   navBar.classList.toggle("active");
 }*/
 
