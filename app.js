23ee9ee718eb751a99c06c78745c543ad3b418c4
Jonathan Jeremiah <110501575+JerryOJJ@users.noreply.github.com>
2023-08-02 01:45:54 +0100

    Updates
    More Info and Less info button in KIT section now works
    
    Read More and less buttons in REVIEWS section now works
    
    Box shadow added to review boxes

--- app.js
+++ app.js
@@ -28,3 +28,64 @@ magnifying_area.addEventListener("mouseleave",function(){
 
   magnifying_img.style.transform = 'translate(-50%, -50%) scale(1)'
 });
+
+function longText() {
+  let moreLongText = document.getElementById("moreLongText");
+  let moreInfo = document.getElementById("moreInfo");
+
+  if (moreLongText.style.display === "none") {
+    moreInfo.innerHTML = "LESS INFO";
+    moreLongText.style.display = "inline";
+  } else {
+    moreInfo.innerHTML = "MORE INFO →";
+    moreLongText.style.display = "none";
+  }
+}
+
+function myFunction() {
+  var dot = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dot.style.display === "none") {
+    dot.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dot.style.display = "none";
+    btnText.innerHTML = "Read Less";
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunctionn() {
+  let dots = document.getElementById("dotss");
+  let moreTexts = document.getElementById("moree");
+  let btnTexts = document.getElementById("myBtnn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnTexts.innerHTML = "Read more";
+    moreTexts.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnTexts.innerHTML = "Read Less";
+    moreTexts.style.display = "inline";
+  }
+}
+
+function read() {
+  let dotss = document.getElementById("dotsss");
+  let moreTextss = document.getElementById("moreee");
+  let btnTextss = document.getElementById("myBtnnn");
+
+  if (dotss.style.display === "none") {
+    dotss.style.display = "inline";
+    btnTextss.innerHTML = "Read more";
+    moreTextss.style.display = "none";
+  } else {
+    dotss.style.display = "none";
+    btnTextss.innerHTML = "Read Less";
+    moreTextss.style.display = "inline";
+  }
+}
